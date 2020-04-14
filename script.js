f81c243d2567eea89f9956acde00a69f65f919c1
Leeangelo Riley <leeangelo.riley@gmail.com>
2020-04-14 08:25:44 -0400

    Check point Week 10
    Used an app to push Week 10 checkpoint

---
+++ script.js
@@ -0,0 +1,12 @@
+// 1. How is information stored ib ab object?
+
+// Information is stored in object by using the assigning a property to the object and giving the property a value
+
+let me = {
+    name: "Leeangelo Riley"
+    sayHi: () => {
+        console.log(me.name);
+        }
+    };
+    
+me.sayHi
